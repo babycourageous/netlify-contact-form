7011b6e614321135bfedbcc76322958db743f012
Rene Dellefont <renedellefont@gmail.com>
2019-05-16 01:27:57 -0400

    Add postcss config.

---
+++ postcss.config.js
@@ -0,0 +1,23 @@
+const postcssimport = require('postcss-import')
+const precss = require('precss')
+const tailwindcss = require('tailwindcss')('./tailwind.config.js')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: ['./**/*.html', './**/*.njk'],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+})
+
+module.exports = {
+  plugins: [
+    postcssimport,
+    precss,
+    tailwindcss,
+    autoprefixer,
+    cssnano,
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
+}
