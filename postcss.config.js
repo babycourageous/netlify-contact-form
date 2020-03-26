32ef29bbb61e40de21d9c6520d8b1bcb5d6309ab
Rene Dellefont <renedellefont@gmail.com>
2020-03-25 23:58:25 -0400

    Auto format

--- postcss.config.js
+++ postcss.config.js
@@ -8,7 +8,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['./**/*.html', './**/*.njk'],
 
   // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 })
 
 module.exports = {
